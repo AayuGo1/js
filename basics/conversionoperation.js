e7a49c9ab7d1870e0aeb9d6f2a9b2928c0eec0e0
AayuGo1 <kushgoel9998@gmail.com>
2024-12-08 18:58:03 +0000

    cs

---
+++ basics/conversionoperation.js
@@ -0,0 +1,15 @@
+// let score = null
+
+// console.log(typeof score); //to know value of score
+  
+// let valueNumber= Number(score)
+// console.log(typeof valueNumber); //number 
+// console.log(valueNumber); //not a number NaN // gives zero when null given
+//  gives 1 for boolean
+
+let loggedIn = 1
+let booleanIsLogged = Boolean(isLoggedIn)
+
+console.log(booleanIsLogged);
+
+ 
