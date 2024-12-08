1d428eeff0f98e7888d047843e9e9d8a088871cd
AayuGo1 <kushgoel9998@gmail.com>
2024-12-08 17:50:24 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; //treat all js code as newer version 
+
+// alert (3 + 3)
+// we are using nodejs and not browser 
+
+console.log(3+3); 
+
+console.log("aayush");
+
+// code readability should be high
+
+let name = "aayush"
+let age= "12"
+let isLoggedIn ="true"
+let state;
+
+// number =>2 to the power 53 
+// bigint 
+// string => ""
+// boolean =>true or false
+// null => standalone value in javascript => representation of empty value
+// undefined => value not asigned 
+// symbol => to find uniqueness 
+
+
+// object
+
+console.log(typeof "aayush");
+console.log(typeof age);
+console.log(typeof null);
+
+
+
+
+
