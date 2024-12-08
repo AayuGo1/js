2c5ae0c81d73e834f36c36921983aaafa87af529
AayuGo1 <kushgoel9998@gmail.com>
2024-12-08 17:00:10 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "kushgoel9998@gmail.com"
+var accountPassword = "12345"
+accountCity = "ghaziabad"
+let accountState;
+//  accountId = 1
+
+console.log(accountId);
+
+accountEmail = "aayush@gmail.com"
+accountPassword = "aayush"
+accountCity = "banguluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+prefer not to use var bcz of issue in block scope and functikn scope
+*/
