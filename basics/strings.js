8d090a2113851682142c26942519e69fc961eacf
AayuGo1 <kushgoel9998@gmail.com>
2024-12-09 18:45:26 +0000

    string

---
+++ basics/strings.js
@@ -0,0 +1,34 @@
+// const name = "aayush"
+// const repocount = 40
+
+// console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+const gameName = new String('aayush-aay-aa')
+console.log(gameName.split('-')); //string to array conversion
+
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+// const Newstring = gameName.substring(0,3) //the last index is not included
+// console.log(Newstring);
+ 
+// const another= gameName.slice(-1,3)
+// console.log(another);
+
+
+
+// // trimming extra spaces
+// const use= "      aaaaa     "
+// console.log(use);
+// console.log(use.trim());
+
+// //  showcasing replace function
+// const use2= "https://aayush.com/aayush%20goel"
+// console.log(use2.replace('%20','-'));
+
+
+// console.log(use2.includes('aayush'));
+
