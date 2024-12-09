2f7e035e6c4388e7146cecad1251a4582cfe8c2c
AayuGo1 <kushgoel9998@gmail.com>
2024-12-09 12:21:33 +0000

    changws

--- basics/conversionoperation.js
+++ basics/conversionoperation.js
@@ -1,15 +1,39 @@
-// let score = null
+// // let score = null
 
-// console.log(typeof score); //to know value of score
+// // console.log(typeof score); //to know value of score
   
-// let valueNumber= Number(score)
-// console.log(typeof valueNumber); //number 
-// console.log(valueNumber); //not a number NaN // gives zero when null given
+// // let valueNumber= Number(score)
+// // console.log(typeof valueNumber); //number 
+// // console.log(valueNumber); //not a number NaN // gives zero when null given
 //  gives 1 for boolean
 
-let loggedIn = 1
-let booleanIsLogged = Boolean(isLoggedIn)
+// let loggedIn = 1
+// let booleanIsLogged = Boolean(isLoggedIn)
 
-console.log(booleanIsLogged);
+// console.log(booleanIsLogged);
 
- 
+// *******************operations******************
+
+let value =3
+let negValue= -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+
+
+
+
+let str1= "aay"
+let str2="ush"
+
+let  str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(8+4+"1"+2+2);
