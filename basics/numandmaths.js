54ba89622852874e40dbde026b873c3176ddc797
AayuGo1 <kushgoel9998@gmail.com>
2024-12-10 12:39:10 +0000

    maths

---
+++ basics/numandmaths.js
@@ -0,0 +1,31 @@
+const score=100
+console.log(score);
+
+const balance= new Number(9000)
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(4)); //a precision value
+
+const othernum= 23.966
+console.log(othernum.toPrecision(2)); //returns a string containing aa number represented either in exponential or fixed point notation with the specialized num of digits.
+
+const hundreds = 9000000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// ************************maths********************************
+console.log(Math);
+
+console.log(Math.abs(-666));
+console.log(Math.round(4.2));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.min(888,999,555989,999,8,9,9,9,8));
+console.log(Math.max(9999,9,9,9,9,9,94,46,4,66,46,64,9,659,6,));
+console.log(Math.random()); // gives random values btw zero and one
+
+
+
+
+
+
