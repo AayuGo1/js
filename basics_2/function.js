599d7ce5fd0ab459d4346033d0d92838ad8fdbcf
AayuGo1 <kushgoel9998@gmail.com>
2024-12-11 19:18:21 +0000

    function

---
+++ basics_2/function.js
@@ -0,0 +1,48 @@
+function saymyname(){
+    console.log("A");
+    console.log("A");
+    console.log("Y");
+    console.log("U");
+    console.log("S");
+    console.log("H");
+}
+// saymyname()
+ 
+// function to add two numbers
+// function addtwonumbers(number1, number2){
+// console.log(number1+number2);
+// }
+// addtwonumbers(6,76)
+
+
+
+
+function addtwonumbers(number1, number2){
+//     let result=number1+number2
+//     return result
+// // return ke baad kuch kaam ni krta function
+// // kuch print ni hoga
+return number1+number2
+    }
+const result=addtwonumbers(838,848)
+console.log("Result: ",result);
+
+
+
+
+function subtracttwonum(number1, number2){
+    return number1-number2
+}
+const hahaha= subtracttwonum(86,85) 
+console.log(hahaha);
+
+
+
+
+
+
+function userlogin(username){
+return `${username} just logged in`
+}
+
+console.log(userlogin("aayush"));
