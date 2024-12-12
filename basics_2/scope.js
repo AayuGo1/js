238c4489b04b0bc91b0f0ded4e02e01f86cbf1cd
AayuGo1 <kushgoel9998@gmail.com>
2024-12-12 14:57:08 +0000

    scope

---
+++ basics_2/scope.js
@@ -0,0 +1,10 @@
+//
+const b=30
+var c=40
+if (true){
+const a = 20;
+}
+console.log(a);
+console.log(b);
+console.log(c);
+//  the scope of let and const is limited to the function where they are used while var has scope outside the function too
